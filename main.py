b86bfb1500809d570978a38377c89cca1d2c7796
YashSharmaWHJ <69245986+YashSharmaWHJ@users.noreply.github.com>
2022-01-17 18:23:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+a = 512
+b = 624
+c = 314
+
+if (a>b and a>c):
+
+        print("A is the biggest")
+if (b>a and b>c):
+
+        print("B is the biggest")
+if (c>b and c>a):
+
+        print("C is the biggest")
+
